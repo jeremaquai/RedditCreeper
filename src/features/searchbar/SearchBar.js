91df0919b33ef53f6d7522b11a282d198af59bc0
jeremaquai <jeremaquai@gmail.com>
2023-01-12 00:38:44 -0600

    first commit

---
+++ src/features/searchbar/SearchBar.js
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+import { addSearchTerm } from "./searchSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { selectSearchTerm } from "./searchSlice";
+
+
+export default function SearchBar() {
+
+    const dispatch = useDispatch();
+    const [searchTerm, setSearchTerm] = useState('');
+    
+    const handleSubmit=() => {
+        
+        dispatch(addSearchTerm(searchTerm));
+    }
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value);
+    }
+
+    return (
+        <form
+            onSubmit={handleSubmit}>
+            <input 
+                type='text'
+                onChange={handleChange}
+            />
+            <input 
+                type='submit'
+                value='Search' 
+            />
+        </form>
+    );
+}
