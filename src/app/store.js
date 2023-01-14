d3d5bf8c71979fa603bffb728e95700b94545937
jeremaquai <jeremaquai@gmail.com>
2023-01-14 15:13:23 -0600

    finalized

--- src/app/store.js
+++ src/app/store.js
@@ -2,11 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import searchReducer from '../features/searchbar/searchSlice';
 import subRedditsReducer from '../features/subReddits/subRedditsSlice';
+import postsReducer from '../features/posts/postsSlice';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     search: searchReducer,
     subReddits: subRedditsReducer,
+    posts: postsReducer,
   },
 });
