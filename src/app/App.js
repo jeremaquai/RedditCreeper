e875139e2f7ced05506e053d00f54c8f549992a1
jeremaquai <jeremaquai@gmail.com>
2023-01-14 15:12:47 -0600

    posts feature finalized for now

--- src/app/App.js
+++ src/app/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 import SubReddits from '../features/subReddits/SubReddits';
+import Posts from '../features/posts/Posts';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Header />
       <div className='homePage'>
         <SubReddits />
+        <Posts />
       </div>
       <Footer />
     </div>
